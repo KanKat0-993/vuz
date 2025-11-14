935ed59f6dd6ac7f17f58a34d84b33bdf7039495
KanKat0-993 <n2rsf2nmkd@privaterelay.appleid.com>
2025-11-14 17:36:18 +0300

    Add files via upload

---
+++ _1.py
@@ -0,0 +1,24 @@
+#1
+'''print('hello world')'''
+
+#2
+'''n = int(input("ur number: "))
+
+f = 1
+for x in range(1, n+1):
+    f *= x
+print("factorial: ", f)'''
+
+#3
+'''m = [3,1,2,5]
+print(max(m))'''
+
+#4
+'''n = [1,2,3,4,5]
+
+n.append(6)
+n.extend([7, 8])
+
+print("all numbers: ", n)
+print("first number: ", n[0])
+print("last number: ", n[-1])'''
